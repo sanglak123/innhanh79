ea6098dde7b599bb343da0c996f0754fd450f0e7
innhanh <qcinnhanh7979@gmail.com>
2023-04-11 14:28:11 +0700

    update 1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,60 @@
 import React from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Form,
+  Button,
+} from "react-bootstrap";
 
 const InNhanh_Header = () => {
-  return <div>InNhanh_Header</div>;
+  return (
+    <div id="innhanh_header" className="w-95">
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <div className="logo">
+              <span className="material-symbols-outlined">settings</span>
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">In Bản Vẽ</Nav.Link>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default InNhanh_Header;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,21 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <title>INNHANH79 || IN BẢN VẺ || IN QUẢNG CÁO || IN KTS</title>
+      <meta name="description" content="IN KTS || IN QUẢNG CÁO || IN BẢN VẼ" />
+
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
+
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+      />
+      <link ref="stylesheet" href="/css/main.css" />
       <body>
         <Main />
         <NextScript />
